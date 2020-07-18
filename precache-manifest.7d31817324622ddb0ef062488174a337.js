8b185dc2726626b9ae6fccaa029fe529a62d7e1b
animeshjain2 <animeshjain67@gmail.com>
2020-07-18 16:07:17 +0530

    Updates

---
+++ precache-manifest.7d31817324622ddb0ef062488174a337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc5ca14cb37021e15ee78d91346712a",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c946b72095abb6b9f2fd",
+    "url": "/Robofriends/static/css/main.4658a7d9.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/Robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c946b72095abb6b9f2fd",
+    "url": "/Robofriends/static/js/main.40e13286.chunk.js"
+  },
+  {
+    "revision": "a6103a62c81f6271b765",
+    "url": "/Robofriends/static/js/runtime-main.79cc7977.js"
+  }
+]);
